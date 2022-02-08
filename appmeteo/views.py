5ed8348e8ff077ae997da648cd8f8017ba01d041
sammyjoo <sammy-jo@hotmail.fr>
2022-02-08 11:57:33 +0100

    Modification dans page equipe et les listes

--- appmeteo/views.py
+++ appmeteo/views.py
@@ -1,7 +1,14 @@
 #slide 47 du cour de python pour les commentaires  
 from flask import *
 
+#importation de l'heure
+import datetime
+
+
 app = Flask (__name__)
+
+#importation des variables du fichier config.py
+app.config.from_object('config')
 
 @app.route('/')
 def index():
@@ -9,10 +16,13 @@ def index():
     data = {}
 
     data["nom_projet"] = "La météo dans les villes du monde !"
-
     data["titre_seo"]="Accueil - Météo"
-
     data["introduction"] = "Vous êtes dans le site de la meilleur météo Monde"
+    
+    #configuration de l'heure actuelle
+    time = datetime.datetime.now()
+    data["heure_actuelle"] = time.hour
+
     return render_template('index.html', data=data)
 
     
@@ -23,10 +33,12 @@ def equipe():
     data = {}
 
     data["nom_equipe"] = "Voici l'equipe"
-
     data["titre_seo"]="Equipe"
-
+    data["nom_de_la_societe"] = app.config['NOM_SOCIETE']
     data["ontro_equipe"] = "Vous êtes dans le site de la meilleur météo Monde"
+
+    data["developpeurs"] = ["alexandre", "laureen", "jojo le plus beau"]
+
     return render_template('equipe.html', data=data)
 
     
--- run.py
+++ run.py
@@ -1,3 +1,4 @@
+#source venv/bin/activate pour activé dans le terminal 
 from appmeteo import app
 if __name__ == "__main__":
     app.run(debug=True)
